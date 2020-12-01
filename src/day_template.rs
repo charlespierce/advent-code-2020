102437c0d0d4836d5b0aa87741ee3666d3ad5030
Charles Pierce <cpierce.grad@gmail.com>
2020-11-30 21:21:51 -0800

    Add template for days

---
+++ src/day_template.rs
@@ -0,0 +1,11 @@
+use aoc_runner_derive::{aoc, aoc_generator};
+
+#[aoc_generator(day2)]
+pub fn parser(input: &str) -> Vec<u32> {
+    todo!();
+}
+
+#[aoc(day2, part1)]
+pub fn solve_part1(input: &[u32]) -> u32 {
+    todo!();
+}
